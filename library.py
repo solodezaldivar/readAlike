b52310df2c600ea749724a9753792b66127aeade
maettuu <mangori41@bluewin.ch>
2024-11-10 16:58:20 +0100

    adjust method order

--- library.py
+++ library.py
@@ -8,8 +8,13 @@ class Book:
         self.authors = authors
         self.categories = categories
 
-    def get_combined_data(self) -> str:
-        return f"{self.title} <.> {self.description} <.> {' '.join(self.authors)} <.> {' '.join(self.categories)}"
+    def __str__(self):
+        return (
+            f"Title: {self.title}\n"
+            f"Description: {self.description}\n"
+            f"Authors: {', '.join(self.authors)}\n"
+            f"Categories: {', '.join(self.categories)}"
+        )
 
     def __eq__(self, other) -> bool:
         return (
@@ -19,18 +24,21 @@ class Book:
                 self.categories == other.categories
         )
 
-    def __str__(self):
-        return (
-            f"Title: {self.title}\n"
-            f"Description: {self.description}\n"
-            f"Authors: {', '.join(self.authors)}\n"
-            f"Categories: {', '.join(self.categories)}"
-        )
+    def get_combined_data(self) -> str:
+        return f"{self.title} <.> {self.description} <.> {' '.join(self.authors)} <.> {' '.join(self.categories)}"
 
 
 class Library:
     def __init__(self, df: DataFrame):
         self.books = self.__df_to_books(df)
+
+    def __str__(self):
+        return (
+            "\n\n".join([
+                f"Book: {idx + 1}\n"
+                f"{str(book)}" for idx, book in enumerate(self.books)
+            ])
+        )
 
     @staticmethod
     def __df_to_books(df: DataFrame) -> list[Book]:
@@ -44,11 +52,3 @@ class Library:
         if book in self.books:
             return self.books.index(book)
         return None
-
-    def __str__(self):
-        return (
-            "\n\n".join([
-                f"Book: {idx + 1}\n"
-                f"{str(book)}" for idx, book in enumerate(self.books)
-            ])
-        )
